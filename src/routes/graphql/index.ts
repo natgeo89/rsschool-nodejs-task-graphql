4c7e844c84f1f1a29e15aba0e27087584d25405c
Viktar <natgeogr93@gmail.com>
2024-11-01 23:04:02 +0300

    add depth validation test

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,11 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { graphql, GraphQLObjectType, GraphQLSchema, parse, validate } from 'graphql';
 import { memberTypeField, memberTypesField } from './memberTypes.js';
 import { POST, POSTS } from './posts.js';
 import { PROFILE, PROFILES } from './profiles.js';
 import { USER, USERS } from './users.js';
 import { GQLContext } from './types/general.js';
+import depthLimit from 'graphql-depth-limit';
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType<unknown, GQLContext>({
@@ -39,6 +40,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const document = parse(req.body.query);
+      const validationErrors = validate(schema, document, [depthLimit(5)]);
+
+      if (validationErrors.length > 0) {
+        return { errors: validationErrors };
+      }
+
       return graphql({
         schema: schema,
         source: req.body.query,
